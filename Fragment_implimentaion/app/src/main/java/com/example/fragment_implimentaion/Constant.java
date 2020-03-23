f1dba4535578ff8275dd227cc46c60362f57ebb7
Prasad Borase <Prasadb@WinjitTechnologiesPvtLtd.local>
2020-03-23 17:49:46 +0530

    Fragment Implementation

---
+++ Fragment_implimentaion/app/src/main/java/com/example/fragment_implimentaion/Constant.java
@@ -0,0 +1,10 @@
+package com.example.fragment_implimentaion;
+
+public class Constant {
+
+
+        public static final String KEY_ALL_FRAG_DATA="ALLfragments";
+
+
+
+}
---
+++ Fragment_implimentaion/app/src/main/java/com/example/fragment_implimentaion/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.fragment_implimentaion;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    public static FragmentManager fragementManager;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fragementManager = getSupportFragmentManager();
+        FragmentTransaction ft =fragementManager.beginTransaction();
+        ft.add(R.id.f1,new personal(),"1");
+        ft.commit();
+
+
+
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        ;
+    }
+}
